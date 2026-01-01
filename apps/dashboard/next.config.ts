231c83cdfb3e7ac9d7cfcf5b0f770a608d03597a
Lozlink <mark.mekhayl@gmail.com>
2026-01-01 23:15:16 +1100

    I FUCKING HATE MONOREPOS

--- apps/dashboard/next.config.ts
+++ apps/dashboard/next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  distDir: '/dist/apps/dashboard',
   transpilePackages: ['@complii/sdk'],
   typescript: {
     ignoreBuildErrors: false,
