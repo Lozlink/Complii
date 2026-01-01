78f3a24d985264d37b0c9c93d3582a73cac0bd0c
Lozlink <mark.mekhayl@gmail.com>
2026-01-01 23:12:22 +1100

    I FUCKING HATE MONOREPOS

--- apps/api/next.config.ts
+++ apps/api/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 //import './ngrok.config'
 
 const nextConfig: NextConfig = {
-  distDir: '../dist/apps/api',
+  distDir: '/dist/apps/api',
   /* config options here */
 };
 
